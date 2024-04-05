095ae7c14ea1a98133c213f5107b871eecb0994e
m7kvqbe1 <48086589+m7kvqbe1@users.noreply.github.com>
2024-04-05 15:31:29 +0100

    Only exit on interupt signal

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/m7kvqbe1/unix-sockets-play/client"
@@ -15,4 +18,8 @@ func main() {
 	time.Sleep(1 * time.Second)
 
 	client.StartUnixSocketClient(socketPath)
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	<-c
 }
